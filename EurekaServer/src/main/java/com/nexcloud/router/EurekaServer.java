a94d109a8bb535267b0c37b87db2c4ae7eabf20d
ssipflow <ssipflow@gmail.com>
2018-10-03 12:01:13 +0900

    Modify line

--- EurekaServer/src/main/java/com/nexcloud/router/EurekaServer.java
+++ EurekaServer/src/main/java/com/nexcloud/router/EurekaServer.java
@@ -1,14 +1,10 @@
 package com.nexcloud.router;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.nexcloud.router.service.RegisterExecutor;
 
